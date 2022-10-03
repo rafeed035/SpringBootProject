2b0f4f6dc4f3c4de0cca5c10bb94a2f9a6c4719d
rafeed035 <shahed.rafeed@gmail.com>
2022-10-03 13:32:54 +0600

    lombok added

--- src/main/java/com/rafeed/springbootdemoproject/Entity/Department.java
+++ src/main/java/com/rafeed/springbootdemoproject/Entity/Department.java
@@ -1,9 +1,18 @@
 package com.rafeed.springbootdemoproject.Entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Department {
 
     @Id
@@ -42,59 +51,4 @@ public class Department {
             nullable = false
     )
     private String departmentCode;
-
-    public Department() {
-    }
-
-    public Department(int departmentId,
-                      String departmentName,
-                      String departmentAddress,
-                      String departmentCode) {
-        this.departmentId = departmentId;
-        this.departmentName = departmentName;
-        this.departmentAddress = departmentAddress;
-        this.departmentCode = departmentCode;
-    }
-
-    public int getDepartmentId() {
-        return departmentId;
-    }
-
-    public void setDepartmentId(int departmentId) {
-        this.departmentId = departmentId;
-    }
-
-    public String getDepartmentName() {
-        return departmentName;
-    }
-
-    public void setDepartmentName(String departmentName) {
-        this.departmentName = departmentName;
-    }
-
-    public String getDepartmentAddress() {
-        return departmentAddress;
-    }
-
-    public void setDepartmentAddress(String departmentAddress) {
-        this.departmentAddress = departmentAddress;
-    }
-
-    public String getDepartmentCode() {
-        return departmentCode;
-    }
-
-    public void setDepartmentCode(String departmentCode) {
-        this.departmentCode = departmentCode;
-    }
-
-    @Override
-    public String toString() {
-        return "Department{" +
-                "departmentId=" + departmentId +
-                ", departmentName='" + departmentName + '\'' +
-                ", departmentAddress='" + departmentAddress + '\'' +
-                ", departmentCode='" + departmentCode + '\'' +
-                '}';
-    }
 }
